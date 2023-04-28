1aca17f27d3fa209295a268bd5b09f8455c7a8ea
manoranjan <baskeymanoranjan400@gmail.com>
2023-04-28 20:32:14 +0530

    changed

--- mano23.c
+++ mano23.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main()
 {
-    char str[10]="ram";
-    printf("%c",str);
-    puts("changes done");
+    char ch[100];
+    puts("enter your name");
+    gets(ch);
+    FILE *mano=fopen("f01.txt","w");
+    if(mano==NULL)
+    {
+        puts("file didn't opened");
+        exit(1);
+    }
+    for(int i=0;i<strlen(ch);i++)
+    {
+        fputc(ch[i],mano);
+    }
+    fclose(mano);
     return 0;
 }
